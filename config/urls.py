bbebefa60bac0295bc92c4e994889e9d7d09ded8
Giovana Malaia <giovanamalaia@gmail.com>
2026-04-21 14:22:53 -0300

    feat: implement password reset functionality with email notifications

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,34 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     
     path('', include('core.urls')),
+
+    path('senha/recuperar/', auth_views.PasswordResetView.as_view(
+        template_name='core/auth/password_reset_form.html',
+        html_email_template_name='core/auth/password_reset_email.html',
+        subject_template_name='core/auth/password_reset_subject.txt',
+        success_url=reverse_lazy('password_reset_done')
+    ), name='password_reset'),
+
+    path('senha/recuperar/enviado/', auth_views.PasswordResetDoneView.as_view(
+        template_name='core/auth/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('senha/recuperar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='core/auth/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete')
+    ), name='password_reset_confirm'),
+
+    path('senha/recuperar/concluido/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='core/auth/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
